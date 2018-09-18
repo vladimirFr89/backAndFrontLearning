e5fdc27acc01284e5b8aa03e6c80eb28416a853b
Федорин Владимир Александрович <fedorin.vladimir@itmh.ru>
2018-09-18 11:16:17 +0500

    Доработан метод удаления и добавлен метод добавления

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -8,12 +8,19 @@ router.get('/', function(req, res, next) {
     res.json(data);
 });
 
-router.get('/:id', function (req, res, next) {
+router.post('/addItem', function(req, res) {
+    console.log('add Item!');
+    console.log(req.body);
+    data.push(req.body);
+    res.json({});
+});
+
+router.get('/getItem/:id', function (req, res, next) {
     const key = 'id';
     if(!req.params.hasOwnProperty(key)) {
         res.json({})
     }
-    console.log('params has a key id with value is: ' + req.params[key]);
+    console.log(`get by id!: params has a key ${key} = ${req.params[key]}`);
     const id = !isNaN(parseInt(req.params[key]))
         ? parseInt(req.params[key])
         : -1;
@@ -21,16 +28,16 @@ router.get('/:id', function (req, res, next) {
     console.log('id = ' + id);
 
     let idxFounded = -1;
-    for (var i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         if(data[i].id === id) {
             idxFounded = i;
         }
     }
 
     if (idxFounded >= 0) {
-        res.json(todos[idxFounded])
+        res.json(data[idxFounded])
     } else {
-        res.json({})
+        res.json({ Message: 'id не найден' })
     }
 });
 
@@ -39,7 +46,7 @@ router.get('/remove/:id', function (req, res, next) {
     if(!req.params.hasOwnProperty(key)) {
         res.json({Message: 'неверный id'});
     }
-    console.log('params has a key id with value is: ' + req.params[key]);
+    console.log(`remove by id!: params has a key ${key} = ${req.params[key]}`);
     const id = !isNaN(parseInt(req.params[key]))
         ? parseInt(req.params[key])
         : -1;
