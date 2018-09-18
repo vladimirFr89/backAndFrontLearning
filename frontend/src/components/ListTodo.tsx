279f4e338432d6be5c4ef256f673cc9b0e48a965
Федорин Владимир Александрович <fedorin.vladimir@itmh.ru>
2018-09-18 12:53:27 +0500

    рефакторинг: вынесение логики axios в отдельный файл

--- frontend/src/components/ListTodo.tsx
+++ frontend/src/components/ListTodo.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-const axios = require('axios');
+import { httpReq } from '../utils'
 import '../styles/main.styl'
 
 import { ToDo } from './ToDo'
@@ -43,21 +43,14 @@ export class ListTodo extends React.Component<IProps, IState> {
     componentDidMount() {
         console.log('componentDidMount');
         //получение списка todos с сервера
-        axios.get('/api/getList')
-            .then((response: any)=> {
-                const targetKey = 'data';
-                if (response && response.hasOwnProperty(targetKey)) {
-                    const todos: APP.TodoItem[] = [...response[targetKey]];
-                    console.log(todos);
-                    this.setState({
-                        list: todos,
-                        uniqueId: this.getUniqueId(todos),
-                    })
-                }
+        httpReq.getList((data: APP.TodoItem[]) => {
+            const todos: APP.TodoItem[] = [...data];
+            console.log(todos);
+            this.setState({
+                list: todos,
+                uniqueId: this.getUniqueId(todos),
             })
-            .catch(function (error: any) {
-                console.log(error);
-            })
+        })
     }
 
     private getUniqueId(list: APP.TodoItem[]) {
@@ -92,97 +85,39 @@ export class ListTodo extends React.Component<IProps, IState> {
         e.preventDefault();
         if (this.state.taskValue.length) {
             const newId = this.state.uniqueId + 1;
-
-            axios.post('/api/getList/addItem', {
+            const newItem: APP.TodoItem = {
                 id: newId,
                 text: this.state.taskValue,
-            })
-                .then((response: any) => {
-                    console.log(response);
-                    //получение списка todos с сервера
-                    axios.get('/api/getList')
-                        .then((response: any)=> {
-                            const targetKey = 'data';
-                            if (response && response.hasOwnProperty(targetKey)) {
-                                const todos = [...response[targetKey]];
-                                console.log(todos);
-                                this.setState({
-                                    list: todos,
-                                    taskValue: '',
-                                    uniqueId: newId,
-                                })
-                            }
-                        })
-                        .catch(function (error: any) {
-                            console.log(error);
-                        })
-                })
-                .catch(function (error: any) {
-                    console.log(error);
-                });
-        }
-    };
+            };
 
-    addItemOld(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        if (this.state.taskValue.length) {
-            const newList = [...this.state.list];
-            const newId = this.state.uniqueId + 1;
-            newList.push(
-                {
-                    id: newId,
-                    text: this.state.taskValue,
-                }
-            );
-            this.setState({
-                list: newList,
-                taskValue: '',
-                uniqueId: newId,
+            httpReq.addItem(newItem, (data: any) => {
+                //получение списка todos с сервера
+                httpReq.getList((data: APP.TodoItem[])=>{
+                    const todos: APP.TodoItem[] = [...data];
+                    console.log(todos);
+                    this.setState({
+                        list: todos,
+                        taskValue: '',
+                        uniqueId: newId,
+                    })
+                })
             })
         }
     };
 
     removeItem(id: number) {
-        axios.get(`/api/getList/remove/${id}`)
-            .then((responce:any) => {
-                console.log(responce);
+        httpReq.removeItem(
+            id,
+            (data: any) => {
                 //получение списка todos с сервера
-                axios.get('/api/getList')
-                    .then((response: any)=> {
-                        const targetKey = 'data';
-                        if (response && response.hasOwnProperty(targetKey)) {
-                            const todos = [...response[targetKey]];
-                            console.log(todos);
-                            this.setState({
-                                list: todos
-                            })
-                        }
+                httpReq.getList((data: APP.TodoItem[])=>{
+                    const todos: APP.TodoItem[] = [...data];
+                    console.log(todos);
+                    this.setState({
+                        list: todos
                     })
-                    .catch(function (error: any) {
-                        console.log(error);
-                    })
-
-            }).catch(function (error: any) {
-            console.log(error)
-        });
-    };
-
-    removeItemOld(id: number) {
-        let deletedIdx = -1;
-        const newList = [...this.state.list];
-        for (let i = 0; i < newList.length; i++) {
-            if (newList[i].id === id) {
-                deletedIdx = i;
-                break;
-            }
-        }
-
-        if (deletedIdx >= 0) {
-            newList.splice(deletedIdx,1);
-            this.setState({
-                list: newList
-            })
-        }
+                });
+            });
     };
 
     render() {
---
+++ frontend/src/utils.ts
@@ -0,0 +1,49 @@
+const axios = require('axios')
+
+export const httpReq = {
+    getList: function (callbackSuccess?: (data: APP.TodoItem[]) => void, callbackNegative?: (error: any)=> void) {
+        axios.get('/api/getList')
+            .then(function (response: any) {
+                console.log(response);
+                if (callbackSuccess) {
+                    callbackSuccess(response.data)
+                }
+            })
+            .catch(function (error: any) {
+                console.log(error);
+            })
+    },
+
+    addItem: function (item: APP.TodoItem, callbackSuccess?: (data: any) => void, callbackNegative?: (error: any)=> void) {
+        axios.post('/api/getList/addItem', {
+            id: item.id,
+            text: item.text,
+        }).then(function (response: any) {
+            console.log(response);
+            if (callbackSuccess) {
+                callbackSuccess(response.data)
+            }
+        }).catch(function (error: any) {
+            console.log(error);
+            if (callbackNegative) {
+                callbackNegative(error)
+            }
+        })
+    },
+
+    removeItem: function (id: number, callbackSuccess?: (data: any) => void, callbackNegative?: (error: any)=> void) {
+        axios.get(`/api/getList/remove/${id}`)
+            .then(function (response: any) {
+                console.log(response);
+                if (callbackSuccess) {
+                    callbackSuccess(response.data)
+                }
+            })
+            .catch(function (error: any) {
+                console.log(error);
+                if (callbackNegative) {
+                    callbackNegative(error)
+                }
+            })
+    }
+};
