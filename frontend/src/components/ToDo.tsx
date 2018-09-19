f2f1df5ad4d636b6b29b94c9986061bbf48f4980
Федорин Владимир Александрович <fedorin.vladimir@itmh.ru>
2018-09-19 11:00:30 +0500

    Избавился от warning

--- frontend/src/components/ToDo.tsx
+++ frontend/src/components/ToDo.tsx
@@ -6,20 +6,30 @@ interface IProps {
     removeItem: (id: number) => void;
 }
 
-export class ToDo extends React.Component<IProps, {}> {
+interface IState {
+    isDone: boolean;
+}
+
+export class ToDo extends React.Component<IProps, IState> {
 
     constructor(props: IProps) {
         super(props);
+        this.state = {
+            isDone: this.props.item.isDone
+        };
         //обработчик установки состояния для задачи
         this.onTaskStatusChange = this.onTaskStatusChange.bind(this);
         //обработчик для кнопки "Удалить"
         this.onRemoveBtnClick = this.onRemoveBtnClick.bind(this);
     }
 
-    onTaskStatusChange = function(e: React.ChangeEvent<HTMLInputElement>) {
+    onTaskStatusChange (e: React.ChangeEvent<HTMLInputElement>) {
         const { item } = this.props;
         item.isDone = !item.isDone;
         this.props.markTaskAsDone(item);
+        this.setState({
+            isDone: item.isDone
+        });
     };
 
     onRemoveBtnClick = function(e: React.MouseEvent<HTMLButtonElement>) {
@@ -29,13 +39,14 @@ export class ToDo extends React.Component<IProps, {}> {
 
     render() {
         const { item } = this.props;
+        console.log(`render todo: id = ${item.id} isDone = ${item.isDone}`);
         return (
             <div>
-                <div className={'taskText ' + (item.isDone ? 'taskText--done' : '')}>{item.text}</div>
+                <div className={'taskText ' + (this.state.isDone ? 'taskText--done' : '')}>{item.text}</div>
                 <input
                     type="checkbox"
                     name="taskState"
-                    checked={item.isDone}
+                    checked={this.state.isDone}
                     onChange={this.onTaskStatusChange}
                 />
                 <button onClick={this.onRemoveBtnClick}>Удалить</button>
